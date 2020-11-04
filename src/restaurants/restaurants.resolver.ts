b4612f8f122b6d5cdbe5ecc57e92746fcf057c3f
loshy <loshy244110@gmail.com>
2020-11-04 21:35:40 +0900

    feat: settings GraphQL

---
+++ src/restaurants/restaurants.resolver.ts
@@ -0,0 +1,10 @@
+import { Resolver, Query } from "@nestjs/graphql";
+
+
+@Resolver()
+export class RestaurantResolver {
+    @Query(returns => Boolean)
+    isPizzaGood(): Boolean {
+        return true
+    }
+}
