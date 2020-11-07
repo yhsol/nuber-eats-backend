6315e391eaa819664b87695e429a54267ea467de
yhsol <yhsol1592@gowid.com>
2020-11-07 17:37:49 +0900

    feat: mutation and validation

--- src/restaurants/dtos/create-restaurant.dto.ts
+++ src/restaurants/dtos/create-restaurant.dto.ts
@@ -1,16 +1,22 @@
 import { Field, ArgsType } from '@nestjs/graphql';
+import { IsString, IsBoolean, Length } from 'class-validator';
 
 @ArgsType()
 export class CreateRestaurantDto {
   @Field(_ => String)
+  @IsString()
+  @Length(5, 10)
   name: string;
 
   @Field(_ => Boolean)
+  @IsBoolean()
   isVegan: boolean;
 
   @Field(_ => String)
+  @IsString()
   address: string;
 
   @Field(_ => String)
+  @IsString()
   ownerName: string;
 }
