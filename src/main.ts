a7631be90ee59ce0f553b9a4be3ef2f81d5f7893
yhsol <loshY244110@gmail.com>
2021-10-17 18:58:25 +0900

    feat: running port

--- src/main.ts
+++ src/main.ts
@@ -6,5 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(4000);
+  console.log('App running in port 4000')
 }
 bootstrap();
